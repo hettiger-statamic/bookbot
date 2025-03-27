59d2fcef5f92d61420d2c36cdde4721c0508c53f
Martin Hettiger <martin@hettiger.com>
2025-03-27 21:06:25 +0100

    Implement num_words function

--- main.py
+++ main.py
@@ -2,7 +2,11 @@ def get_book_text(file_path):
     with open(file_path, "r") as file:
         return file.read()
 
+def num_words(text):
+    return len(text.split())
+
 def main():
-    print(get_book_text("books/frankenstein.txt"))
+    text = get_book_text("books/frankenstein.txt")
+    print(f"{num_words(text)} words found in the document")
 
 main()
