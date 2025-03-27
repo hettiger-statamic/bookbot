ca1ff6735726f11102f924bdf2f6e6dbc84056fb
Martin Hettiger <martin@hettiger.com>
2025-03-27 21:57:01 +0100

    Accept file path as input arg

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import get_num_words, get_char_counts, get_report_data
+import sys
 
 
 def get_book_text(file_path):
@@ -7,7 +8,10 @@ def get_book_text(file_path):
 
 
 def main():
-    file_path = "books/frankenstein.txt"
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    file_path = sys.argv[1]
     text = get_book_text(file_path)
     print("============ BOOKBOT ============")
     print(f"Analyzing book found at {file_path}...")
